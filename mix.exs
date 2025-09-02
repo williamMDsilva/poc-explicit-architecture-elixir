f0551fa98d1b34c7fe17f84662f2fdf2c8d98030
williamMDsilva <william.moreiradasilva@hotmail.com>
2025-09-01 21:06:32 -0300

    dev command

--- mix.exs
+++ mix.exs
@@ -58,6 +58,7 @@ defmodule App.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      dev: ["phx.server"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
